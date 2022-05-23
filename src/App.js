f24a613a858e3ee11ff97b0d1205fbb57424d1e0
affandi001 <rajashahbax4@gmail.com>
2022-05-23 22:19:45 +0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Button from './components/buttonsection';
+import Footer from './components/footer';
 import Grid from './components/grid section';
 import Header from './components/header';
 import Hero from './components/hero';
@@ -7,22 +8,25 @@ import Section1 from './components/section1';
 import Section2 from './components/section2';
 
 
+
 export default function App() {
   return (
 
     <>
-    <div className='bg-[#19191B] '>
-      <Header />
-      <Hero />
-      </div>  
+      <div className='bg-[#19191B] '>
+        <Header />
+        <Hero />
+      </div>
       <Section1 />
       <Section2 />
       <Grid />
       <Button />
-    
-      
-   
-   
+      <Footer />
+
+
+
+
+
     </>
 
   )
